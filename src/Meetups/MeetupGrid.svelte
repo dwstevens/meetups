a57b12c6f5eb246a9ff55c96a9184077001b2e8a
David Stevens <dwstevens@gmail.com>
2020-06-04 11:10:22 -0400

    initial commit; into section 8 of the course

---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,35 @@
+<script>
+	import MeetupItem from './MeetupItem.svelte';
+
+	export let meetups;
+</script>
+
+<style>
+	section {
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-gap: 1rem;
+	}
+
+	@media (min-width: 768px) {
+		section {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+</style>
+
+<section id="meetups">
+	{#each meetups as meetup, i}
+		<MeetupItem
+			id={meetup.id}
+			isFav={meetup.isFavorite}
+			title={meetup.title}
+			subtitle={meetup.subtitle}
+			description={meetup.description}
+			imageUrl={meetup.imageUrl}
+			address={meetup.address}
+			contact={meetup.contactEmail}
+			on:toggleFavorite />
+	{/each}
+</section>
